04d6cfd3049886e5de2978d94b52d72cd1eabb69
afortman <annefortman@g.harvard.edu>
2019-01-23 14:04:28 -0500

    Add files via upload

--- tmvatest.py
+++ tmvatest.py
@@ -1,18 +1,33 @@
 from ROOT import TMVA, TFile, TTree, TCut, TString
 TMVA.Tools.Instance()
+import sys
 
 ##https://nbviewer.jupyter.org/github/iml-wg/tmvatutorials/blob/master/TMVA_DNN.ipynb
 ##https://aholzner.wordpress.com/2011/08/27/a-tmva-example-in-pyroot/
 ##https://root.cern.ch/download/doc/tmva/TMVAUsersGuide.pdf
 
-inputFile_sig = TFile.Open("bigfiles/hazel_sig_smearf.root")
-inputFile_bkg = TFile.Open("bigfiles/hazel_bkg_smearf.root")
-outputFile = TFile.Open("TMVAOutput_1M_smearf.root", "RECREATE")
+
+from optparse import OptionParser
+parser = OptionParser()
+
+parser.add_option('-e', type='string', action='store',
+                  default= '',
+                  dest='eff',
+                  help='hit efficiency')
+
+(options, args) = parser.parse_args()
+argv = []
+
+
+
+inputFile_sig = TFile.Open("efftesting/hazel_both_smearf_1M_35ns_e"+str(options.eff)+".root")
+inputFile_bkg = TFile.Open("efftesting/hazel_bkg_smearf_1M_35ns_e"+str(options.eff)+".root")
+outputFile = TFile.Open("TMVAOutput_sigbkg_e"+str(options.eff)+".root", "RECREATE")
 
 factory = TMVA.Factory("TMVAClassification", outputFile,
                        "!V:!Silent:Color:DrawProgressBar:AnalysisType=Classification" )
 
-loader = TMVA.DataLoader("dataset")
+loader = TMVA.DataLoader("dataset_e"+str(options.eff))
 
 #loader.AddVariable("EventNumHazel",'I')
 #loader.AddVariable("EventNumGingko",'I')
@@ -28,7 +43,7 @@ loader.AddVariable("Hit_plane4",'I')
 loader.AddVariable("Hit_plane5",'I')
 loader.AddVariable("Hit_plane6",'I')
 loader.AddVariable("Hit_plane7",'I')
-loader.AddVariable("Hit_n",'I')
+#loader.AddVariable("Hit_n",'I')
 #loader.AddVariable("dtheta")
 #loader.AddVariable("chi2")
 
@@ -38,6 +53,7 @@ tbackground = inputFile_bkg.Get("hazel")
 loader.AddSignalTree(tsignal)
 loader.AddBackgroundTree(tbackground)
 #loader.PrepareTrainingAndTestTree(TCut(""),"nTrain_Signal=1000:nTrain_Background=1000:SplitMode=Random:NormMode=NumEvents:!V")
+#loader.PrepareTrainingAndTestTree(TCut("Hit_n==8"),"SplitMode=Random:NormMode=NumEvents:!V")
 loader.PrepareTrainingAndTestTree(TCut(""),"SplitMode=Random:NormMode=NumEvents:!V")
 
 # General layout
@@ -65,13 +81,20 @@ dnnOptions.Append(trainingStrategyString)
 
 # Standard implementation, no dependencies.
 stdOptions =  dnnOptions + ":Architecture=CPU"
-factory.BookMethod(loader, TMVA.Types.kDNN, "DNN_1M_smearf", stdOptions)
+factory.BookMethod(loader, TMVA.Types.kDNN, "DNN", stdOptions)
+
+#factory.BookMethod(loader, TMVA.Types.kDNN, "DNN","Architecture=CPU")
 
 ##Boosted Decision Trees
-factory.BookMethod(loader,TMVA.Types.kBDT, "BDT_1M_smearf","!V:NTrees=200:MinNodeSize=2.5%:MaxDepth=2:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20" )
+#factory.BookMethod(loader,TMVA.Types.kBDT, "BDT","!V:NTrees=200:MinNodeSize=2.5%:MaxDepth=2:BoostType=AdaBoost:AdaBoostBeta=0.5:UseBaggedBoost:BaggedSampleFraction=0.5:SeparationType=GiniIndex:nCuts=20" )
+
+factory.BookMethod(loader,TMVA.Types.kBDT, "BDT" )
 
 ##Multi-Layer Perceptron (Neural Network)
-factory.BookMethod(loader, TMVA.Types.kMLP, "MLP_1M_smearf","!H:!V:NeuronType=tanh:VarTransform=N:NCycles=100:HiddenLayers=N+5:TestRate=5:!UseRegulator" )
+factory.BookMethod(loader, TMVA.Types.kMLP, "MLP","!H:!V:NeuronType=tanh:VarTransform=N:NCycles=100:HiddenLayers=N+5:TestRate=5:!UseRegulator" )
+
+factory.BookMethod(loader, TMVA.Types.kKNN, "kNN" );
+
 
 # CPU implementation, using BLAS
 #cpuOptions = dnnOptions + ":Architecture=CPU"
@@ -84,4 +107,4 @@ factory.EvaluateAllMethods()
 
 c = factory.GetROCCurve(loader)
 c.Draw()
-c.SaveAs("roc/roc_TMVA_hits_smearf_1M.pdf")
+c.SaveAs("dataset_e"+str(options.eff)+"/roc_TMVA_1M_35ns_e"+str(options.eff)+".pdf")
